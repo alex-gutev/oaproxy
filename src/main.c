2d32573b7f43b0326d77598b7edb0e02c5e97bd8
Alexander Gutev <alex.gutev@gmail.com>
2021-02-01 21:07:07 +0100

    Initial commit: configure script and Makefile.

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#define GOA_API_IS_SUBJECT_TO_CHANGE
+#include <goa/goa.h>
+
+#include <locale.h>
+
+int main(int argc, char *argv[])
+{
+    /* TODO: Listen on SMTP port and forward to  */
+    return 0;
+}
