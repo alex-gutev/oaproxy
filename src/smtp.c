b7724538afa8358059c40e5bc8351e3657c630a1
Alexander Gutev <alex.gutev@gmail.com>
2021-02-08 14:59:54 +0100

    Refactor command parsing and fix memory leaks.

--- src/smtp.c
+++ src/smtp.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <syslog.h>
 
 #include <sys/types.h>
@@ -22,30 +23,59 @@
 
 #define RECV_BUF_SIZE 1024
 
+#define CMD_AUTH_PLAIN "AUTH PLAIN"
+#define CMD_AUTH_PLAIN_LEN 10
+
+#define CMD_DATA "DATA"
+#define CMD_DATA_LEN 4
+
 /**
- * SMTP command
+ * SMTP State.
+ *
+ * Keeps track of whether in Data or command state.
  */
-typedef enum smtp_command {
+struct smtp_client_state {
+    /* True when in Data state */
+    bool in_data;
+};
+
+/**
+ * SMTP Command ID Constants
+ */
+typedef enum smtp_command_type {
     /* Generic Command */
     SMTP_CMD = 0,
     /* AUTH - Authorization Command */
     SMTP_AUTH = 1,
     /* QUIT - End Session Command */
     SMTP_DATA
-} smtp_command;
+} smtp_command_type;
+
+/**
+ * SMTP Command
+ */
+struct smtp_command {
+    /** Command */
+    smtp_command_type command;
+    /** Command data (following command) */
+    const char *data;
+    /** Command data length */
+    size_t len;
+};
 
 /* Handling SMTP Client Command */
 
 /**
  * Read and handle/forward an SMTP command from the client.
  *
+ * @param state SMTP client state
  * @param c_fd Client socket file descriptor
  * @param s_bio Server BIO object
  *
  * @return true if the command was handled successfully, false
  *   otherwsie.
  */
-static bool smtp_client_handle_cmd(int c_fd, BIO *s_bio);
+static bool smtp_client_handle_cmd(struct smtp_client_state *state, int c_fd, BIO *s_bio);
 
 /**
  * Read an SMTP command from the client.
@@ -58,16 +88,44 @@ static bool smtp_client_handle_cmd(int c_fd, BIO *s_bio);
  */
 static ssize_t smtp_read_client_cmd(int c_fd, char *buf, size_t n);
 
+
+/* Command Parsing */
+
 /**
  * Parse an SMTP command from the client response.
  *
+ * @param state SMTP client state
+ *
  * @param response Client response
- * @param eptr Pointer to the first character after command
+ *
  * @param len Number of bytes in client response
  *
- * @return SMTP command.
+ * @param command Pointer to smtp_command struct. Filled with command
+ *   data on output.
+ *
+ * @return True if command was parsed successfully.
  */
-static smtp_command smtp_parse_command(const char *response, const char **eptr, size_t len);
+static bool smtp_parse_command(struct smtp_client_state *state, const char *response, size_t len, struct smtp_command *command);
+
+/**
+ * Find the start of the command auxiliary data.
+ *
+ * @param data Pointer to first byte following command.
+ * @param size Number of bytes in data.
+ *
+ * @return Pointer to first data byte.
+ */
+static const char *smtp_command_data_start(const char *data, size_t size);
+
+/**
+ * Determine the number of bytes comprising the auxiliary data.
+ *
+ * @param data Pointer to command data.
+ * @param size Number of bytes of remaining response data.
+ *
+ * @return Number of bytes which comprising command data.
+ */
+static size_t smtp_command_data_len(const char *data, size_t size);
 
 /**
  * Parse the username from an SMTP plain auth command.
@@ -92,6 +150,9 @@ static const char * smtp_parse_auth_user(const char *data, size_t n);
  *   false otherwise.
  */
 static bool smtp_auth_client(int fd, BIO *bio, GList *account, const char *user);
+
+
+/* Sending Data */
 
 /**
  * Send data to the server.
@@ -157,6 +218,9 @@ void smtp_handle_client(int c_fd, const char *host) {
     int s_fd = BIO_get_fd(bio, NULL);
     int maxfd = c_fd < s_fd ? s_fd : c_fd;
 
+    struct smtp_client_state client_state;
+    client_state.in_data = false;
+
     while (1) {
         fd_set rfds;
 
@@ -176,7 +240,7 @@ void smtp_handle_client(int c_fd, const char *host) {
                 break;
         }
         else if (FD_ISSET(c_fd, &rfds)) {
-            if (!smtp_client_handle_cmd(c_fd, bio))
+            if (!smtp_client_handle_cmd(&client_state, c_fd, bio))
                 break;
         }
     }
@@ -191,7 +255,7 @@ close_client:
 
 /* Handling SMTP Client Commands */
 
-bool smtp_client_handle_cmd(int c_fd, BIO *s_bio) {
+bool smtp_client_handle_cmd(struct smtp_client_state *state, int c_fd, BIO *s_bio) {
     char c_data[RECV_BUF_SIZE];
     ssize_t c_n = smtp_read_client_cmd(c_fd, c_data, sizeof(c_data));
 
@@ -204,23 +268,31 @@ bool smtp_client_handle_cmd(int c_fd, BIO *s_bio) {
         return false;
     }
 
-    const char *eptr = NULL;
-    smtp_command cmd = smtp_parse_command(c_data, &eptr, c_n);
+    struct smtp_command cmd;
+    smtp_parse_command(state, c_data, c_n, &cmd);
 
-    switch (cmd) {
+    switch (cmd.command) {
     case SMTP_AUTH: {
-        const char *user = smtp_parse_auth_user(eptr, c_n - (eptr - c_data));
+        const char *user = smtp_parse_auth_user(cmd.data, cmd.len);
 
-        GList *accounts = goa_client_get_accounts(get_goaclient(NULL));
-        GList *account = find_goaccount(accounts, user);
+        if (user) {
+            GList *accounts = goa_client_get_accounts(get_goaclient(NULL));
+            GList *account = find_goaccount(accounts, user);
 
-        if (account) {
-            return smtp_auth_client(c_fd, s_bio, account, user);
+            free(user);
+
+            if (account) {
+                bool auth = smtp_auth_client(c_fd, s_bio, account, user);
+                g_list_free_full(accounts, (GDestroyNotify)g_object_unref);
+
+                return auth;
+            }
+
+            syslog(LOG_USER | LOG_WARNING, "Could not find GNOME Online Account for username %s", user);
+
+            g_list_free_full(accounts, (GDestroyNotify)g_object_unref);
         }
-        else {
-            return false;
-        }
-    } break;
+    }
 
     default:
         return smtp_server_send(s_bio, c_data, c_n);
@@ -254,54 +326,78 @@ ssize_t smtp_read_client_cmd(int fd, char *buf, size_t n) {
     return total;
 }
 
-smtp_command smtp_parse_command(const char *response, const char **eptr, size_t len) {
-    if (len > 10) {
-        if (strncasecmp("AUTH PLAIN", response, 10) == 0 &&
-            (response[10] == ' ' || response[10] == '\r' ||
-             response[10] == '\n')) {
-            *eptr = response + 10;
-            return SMTP_AUTH;
-        }
+
+/* Parsing SMTP Commands */
+
+bool smtp_parse_command(struct smtp_client_state *state, const char *response, size_t len, struct smtp_command *command) {
+    if (len > CMD_AUTH_PLAIN_LEN &&
+        strncasecmp(CMD_AUTH_PLAIN, response, CMD_AUTH_PLAIN_LEN) == 0 &&
+        (response[CMD_AUTH_PLAIN_LEN] == ' ' ||
+         response[CMD_AUTH_PLAIN_LEN] == '\r' ||
+         response[CMD_AUTH_PLAIN_LEN] == '\n')) {
+
+        command->command = SMTP_AUTH;
+        command->data = smtp_command_data_start(response + CMD_AUTH_PLAIN_LEN, len - CMD_AUTH_PLAIN_LEN);
+        command->len = smtp_command_data_len(command->data, (response + len) - command->data);
+
+        return true;
     }
 
-    if (len > 4) {
-        if (strncasecmp("DATA", response, 4) == 0 &&
-                 response[4] == '\r' || response[4] == '\n') {
-            return SMTP_DATA;
-        }
+    if (len > CMD_DATA_LEN &&
+        strncasecmp(CMD_DATA, response, CMD_DATA_LEN) == 0 &&
+        (response[CMD_DATA_LEN] == ' ' ||
+         response[CMD_DATA_LEN] == '\r' ||
+         response[CMD_DATA_LEN] == '\n')) {
+
+        command->command = SMTP_DATA;
+        command->data = NULL;
+        command->len = 0;
     }
 
-    *eptr = NULL;
-    return SMTP_CMD;
+    command->command = SMTP_CMD;
+    command->data = NULL;
+    command->len = 0;
+
+    return true;
+}
+
+const char *smtp_command_data_start(const char *data, size_t size) {
+    while (size-- && *data == ' ') {
+        data++;
+    }
+
+    return data;
+}
+
+size_t smtp_command_data_len(const char *data, size_t size) {
+    size_t len = 0;
+
+    while (size--) {
+        if (*data == '\r' || *data == '\n')
+            return len;
+
+        data++;
+        len++;
+    }
+
+    return len;
 }
 
 const char * smtp_parse_auth_user(const char *data, size_t n) {
-    if (data[0] == ' ' && n > 1) {
-        data++;
-        n--;
-    }
-
-    size_t b_len = 0;
-
-    // Find end of command
-    while (n && data[b_len] != '\r' && data[b_len] != '\n') {
-        b_len++;
-        n--;
-    }
-
-    char *dec = base64_decode(data, &b_len);
+    char *dec = base64_decode(data, &n);
+    if (!dec) return NULL;
 
     // Find first NUL character
     size_t user_start = 0;
-    while (b_len && dec[user_start++]) {
-        b_len--;
+    while (n && dec[user_start++]) {
+        n--;
         user_start++;
     }
 
     // Find end of username
     size_t pass_start = user_start;
-    while (b_len && dec[pass_start++]) {
-        b_len--;
+    while (n && dec[pass_start++]) {
+        n--;
     }
 
     size_t user_len = pass_start - user_start;
